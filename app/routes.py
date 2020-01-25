245945cb3f0bab557f42011f160821d1a687155f
Anish Sharma <aneeshsharma15@outlook.com>
2020-01-25 16:49:40 +0530

    Added sample form

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
 from app import app
+from flask import request
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello World'
+
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    try:
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            return username + " | " + password
+        return "Invalid Request"
+    except Exception as e:
+        return "Exception : " + str(e)
